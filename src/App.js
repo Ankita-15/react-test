1c3df259e3cefdfc81ea84b88a233bc2b5145148
Ankita-15 <ankita.15137@gmail.com>
2021-03-03 16:18:19 +0530

    added register module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './components/login.js'
+import Register from './components/register.js'
 
 function App() {
   return (
     <div className="App">
 		<Login />
+    <Register />
     </div>
   );
 }
